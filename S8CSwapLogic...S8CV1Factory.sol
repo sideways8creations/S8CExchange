753898dc1daee7e64f7764fb3758372ec546821d
Jon Dreksler <jondreksler@sideways8creations.com>
2021-08-25 14:51:57 -0700

    Folders in place

---
+++ S8CSwapLogic...S8CV1Factory.sol
@@ -0,0 +1,49 @@
+pragma solidity =0.5.16;
+
+import './interfaces/S8CV1Factory.sol';
+import './S8CV1Pair.sol';
+
+contract S8CV1Factory is IS8CV1Factory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'S8CV1: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'S8CV1: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'S8CV1: PAIR_EXISTS'); // One check needed
+        bytes memory bytecode = type(S8CV1Pair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IUniswapV2Pair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // Map pop in reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'S8CV1: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'S8CV1: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+}
